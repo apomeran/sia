5a136445caeaf752e1378f47c513382205244311
Alan Pomerantz <alan_pomerantz@hotmail.com>
2015-03-28 12:53:01 -0300

    Updated Test

--- TP1/model/src/main/java/edu/itba/sia/Test.java
+++ TP1/model/src/main/java/edu/itba/sia/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Tile tile = new Tile(218301697);
 		System.out.println(tile.toString());
+		tile = new Tile(13, 3, 5, 1);
+		System.out.println(tile.toString());
 	}
 }
